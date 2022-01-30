2c6763c4b31439a1822272de389e91b8f19930ee
rajkumar888 <Soma@rajeev67>
2022-01-30 15:13:41 +0800

    demo commit first time

---
+++ src/test/java/comdemo/DemoFile.java
@@ -0,0 +1,16 @@
+package comdemo;
+
+import java.util.Date;
+
+import org.testng.annotations.Test;
+
+public class DemoFile {
+
+	@Test()
+	public void demotestmavengit() {
+
+		System.out.println(".....demotestmavengit at " + new Date() + ".......");
+
+	}
+
+}
